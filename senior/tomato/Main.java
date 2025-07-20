7a9372f5b7d09f6e9c4536b589ce33981b73f92a
v.holovko <codeine200@gmail.com>
2025-07-20 18:39:45 +0300

    init

--- senior/tomato/Main.java
+++ senior/tomato/Main.java
@@ -1,5 +1,85 @@
+package senior.tomato;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ddddd");
+
+        MyCustomThreadPool pool = new MyCustomThreadPool(2);
+        pool.submit(() -> {
+            System.out.println("Start Task 1");
+            sleep(1500);
+            System.out.println("End Task 1");
+        });
+
+        pool.submit(() -> {
+            System.out.println("Start Task 2");
+            sleep(1500);
+            System.out.println("End Task 2");
+        });
+
+        pool.submit(() -> {
+            System.out.println("Start Task 3");
+            sleep(500);
+            System.out.println("End Task 3");
+        });
+        pool.shutdownNow();
+
+        sleep(2000);
+        System.exit(0);
+
+        Thread task1 = new Thread(() -> {
+            System.out.println("Start Task 1");
+            sleep(1500);
+            System.out.println("End Task 1");
+        });
+
+        Thread task2 = new Thread(() -> {
+            System.out.println("Start Task 2");
+            sleep(1500);
+            System.out.println("End Task 2");
+        });
+
+        Thread task3 = new Thread(() -> {
+            System.out.println("Start Task 3");
+            sleep(1500);
+            System.out.println("End Task 3");
+        });
+
+        task1.start();
+        task2.start();
+        task3.start();
+
+        sleep(2000);
+
+        System.exit(0);
+
+        Executor executor = Executors.newFixedThreadPool(2);
+        executor.execute(() -> {
+            System.out.println("Start Task 1");
+            sleep(1500);
+            System.out.println("End Task 1");
+        });
+
+        executor.execute(() -> {
+            System.out.println("Start Task 2");
+            sleep(1000);
+            System.out.println("End Task 2");
+        });
+        executor.execute(() -> {
+            System.out.println("Start Task 3");
+            sleep(10);
+            System.out.println("End Task 3");
+        });
     }
+
+    private static void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- senior/tomato/MyCustomThreadPool.java
+++ senior/tomato/MyCustomThreadPool.java
@@ -1,4 +1,69 @@
 package senior.tomato;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class MyCustomThreadPool {
+    private List<Worker> workers = new ArrayList<>();
+    private final List<Runnable> tasks = new LinkedList<>();
+    private boolean isShutdown = false;
+
+    public MyCustomThreadPool(int poolSize) {
+        for (int i=0; i<poolSize; i++) {
+            Worker worker = new Worker("thread#" + (i + 1));
+            workers.add(worker);
+            worker.start();
+        }
+    }
+
+    public void submit(Runnable task) {
+        synchronized (tasks) {
+            tasks.addLast(task);
+            tasks.notify();
+        }
+    }
+
+    public void shutdownNow() {
+        isShutdown = true;
+        synchronized (tasks) {
+            tasks.notifyAll(); // разбудим все потоки
+        }
+        for (Worker worker : workers) {
+            worker.interrupt();
+        }
+    }
+
+    class Worker extends Thread {
+
+        public Worker(String name) {
+            super(name);
+        }
+
+        @Override
+        public void run () {
+            while(true) {
+                Runnable task;
+                synchronized (tasks) {
+                    while (tasks.isEmpty() && !isShutdown) {
+                        try {
+                            tasks.wait();
+                        } catch (InterruptedException e) {
+                            return;
+                        }
+                    }
+                    if (isShutdown && tasks.isEmpty()) {
+                        return;
+                    }
+                    task = tasks.removeFirst();
+                }
+                try {
+                    System.out.println(getName() + " works");
+                    task.run();
+                } catch (Exception e) {
+                    System.out.println("Error");
+                }
+            }
+        }
+    }
 }
